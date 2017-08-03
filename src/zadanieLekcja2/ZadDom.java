47053fda6f3919bc98dd0fd2bfd264c60d977ff8
rima <rima@PC626>
2017-08-03 15:39:02 +0200

    Rozwiązanie ZadDom2

---
+++ src/zadanieLekcja2/ZadDom.java
@@ -0,0 +1,12 @@
+package zadanieLekcja2;
+
+public class ZadDom {
+
+	public static void main(String[] args) {
+		Zmienne2 zmienne = new Zmienne2();
+		zmienne.setMiasto("Gdynia");
+		
+System.out.println("Miasto: " + zmienne.getMiasto());
+	}
+
+}
---
+++ src/zadanieLekcja2/Zmienne2.java
@@ -0,0 +1,15 @@
+package zadanieLekcja2;
+
+public class Zmienne2 {
+	private String miasto;
+
+	public String getMiasto() {
+		return miasto;
+	}
+
+	public void setMiasto(String miasto) {
+		this.miasto = miasto;
+	}
+	
+
+}
