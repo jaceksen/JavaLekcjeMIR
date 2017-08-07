ae4d3e80ea74a7b47fca26c710db483a0ef98225
rima <rima@PC626>
2017-08-07 23:34:54 +0200

    zrobione zadanie domowe 4

---
+++ src/zadDomLekcja4/Klasa1.java
@@ -0,0 +1,13 @@
+package zadDomLekcja4;
+
+public class Klasa1 {
+
+	public static void main(String[] args) {
+		Klasa2 zadanie4 = new Klasa2(39, "Zenon");
+		zadanie4.metoda();
+		System.out.println(zadanie4.wynikZ());
+		System.out.println("Lata *7 = " + zadanie4.lata7());
+
+	}
+
+}
